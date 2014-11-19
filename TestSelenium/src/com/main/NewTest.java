c12f59052f28a2cb12e611e065afcf7613926656
panther999 <pragatirpatra@gmail.com>
2014-11-20 02:09:19 +0530

    new changes

--- TestSelenium/src/com/main/NewTest.java
+++ TestSelenium/src/com/main/NewTest.java
@@ -5,22 +5,29 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.TestNG;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
+
+import outputreports.Log;
 
 import com.core.BrowserWrapper;
 
 public class NewTest {
 	BrowserWrapper DriverMain = new BrowserWrapper();
 	WebDriver Driver;
-	Properties config;
+	public static Properties configprop= new Properties();
 
 	  
   @Test
-  public void f() {
+  public void homepage() {
+	  
+	  
   }
   
   
@@ -30,11 +37,19 @@ public class NewTest {
   
   @BeforeClass
   public void beforeClass(String browser) throws FileNotFoundException, IOException {
+	  DOMConfigurator.configure("log4j.xml");
+	  try{
 	  Driver = DriverMain.startbrowser(browser.toString());
-	  Properties configprop = new Properties();
 	  configprop.load(new FileInputStream("src\\com\\config\\config.properties"));
 	  Driver.get(configprop.getProperty("DEFAULTURL"));
-	  Driver.manage().window().maximize(); 
+	  Driver.manage().window().maximize();
+	  Log.info("Started webdriver for \"" + browser + "\" successfully");
+	  Reporter.log("Started webdriver for \"" + browser + "\" successfully",true);
+	  }
+	  catch(Exception e){
+	  Log.error("Unable to start webdriver. ERROR : " + e.getMessage());
+	  Reporter.log("Unable to start webdriver. ERROR : " + e.getMessage(),true);
+	  }
   }
 
   @AfterClass
