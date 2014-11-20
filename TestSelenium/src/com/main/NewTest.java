952ef5159ff1538efb4f3bb3b6ca6a5f3e9ef300
panther999 <pragatirpatra@gmail.com>
2014-11-21 02:12:49 +0530

    implemented awt

--- TestSelenium/src/com/main/NewTest.java
+++ TestSelenium/src/com/main/NewTest.java
@@ -17,17 +17,23 @@ import org.testng.annotations.AfterClass;
 import outputreports.Log;
 
 import com.core.BrowserWrapper;
+import com.gui.guiwin;
+import com.utility.Excelutils;
 
 public class NewTest {
 	BrowserWrapper DriverMain = new BrowserWrapper();
 	WebDriver Driver;
 	public static Properties configprop= new Properties();
+	guiwin windowst = new guiwin();
 
 	  
   @Test
-  public void homepage() {
-	  
-	  
+  public void homepage() throws InterruptedException {
+	 
+		Thread.sleep(2000);
+	
+	  Excelutils ex= new Excelutils();
+	  System.out.println(ex.readdatafromxl("homepage", "pathtype", 2));
   }
   
   
@@ -36,15 +42,21 @@ public class NewTest {
   @Parameters("browser")
   
   @BeforeClass
-  public void beforeClass(String browser) throws FileNotFoundException, IOException {
+  public void beforeClass(String browser) throws FileNotFoundException, IOException, InterruptedException {
 	  DOMConfigurator.configure("log4j.xml");
+	  windowst.createstatusbox("Here we go :)");
+	  
 	  try{
+	  windowst.changestatus("trying to open browser");
 	  Driver = DriverMain.startbrowser(browser.toString());
 	  configprop.load(new FileInputStream("src\\com\\config\\config.properties"));
+	  
 	  Driver.get(configprop.getProperty("DEFAULTURL"));
 	  Driver.manage().window().maximize();
+	  windowst.changestatus("Started webdriver for \"" + browser + "\" successfully");
 	  Log.info("Started webdriver for \"" + browser + "\" successfully");
 	  Reporter.log("Started webdriver for \"" + browser + "\" successfully",true);
+	  
 	  }
 	  catch(Exception e){
 	  Log.error("Unable to start webdriver. ERROR : " + e.getMessage());
@@ -54,8 +66,20 @@ public class NewTest {
 
   @AfterClass
   public void afterClass() {
+	  try{
+	 windowst.changestatus("Closing webdriver.");
 	  Driver.close();
 	  Driver.quit();
+	  
+	  Log.error("Closed webdriver");
+	  Reporter.log("Closed webdriver");
+	  }
+	  catch(Exception e){
+		  windowst.changestatus("Unable to close webdriver. ERROR : " + e.getMessage());
+		  Log.error("Unable to close webdriver. ERROR : " + e.getMessage());
+		  Reporter.log("Unable to close webdriver. ERROR : " + e.getMessage(),true);
+		  }
+	  
   }
 
 }
